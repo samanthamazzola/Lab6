e75aa6b98c69fc7da7ee1825c34f5bd955ea1f48
samantha <33375727+samanthamazzola@users.noreply.github.com>
2018-07-24 14:53:30 -0400

    Update Program.cs

--- Lab6/Lab6/Program.cs
+++ Lab6/Lab6/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace Lab6
 {
@@ -6,7 +6,29 @@ namespace Lab6
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int sidesCount = 6;
+            random = new Random();
+
+            Rollingdie dice1 = new Rollingdie(10);
+            Console.WriteLine(die1.GetSides());
+            Rollingdie dice2 = new Rollingdie();
+            Console.WriteLine(die2.GetSides());
+           
+
+        }
+        public static int Rollingdie(int sidesCount)
+        {
+            sidesCount = GetSides();
+            random = new Random();
+
+        }
+        public static int GetSides();
+            {
+                return sidesCount;
+            }
+        public int Roll()
+        {
+            return random.Next(1, sidesCount + 1);
         }
     }
 }
